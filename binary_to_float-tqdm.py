ecadc1cb64e433ac108b417f1edd47362f60fa72
alex-meadows <71044875+alex-meadows@users.noreply.github.com>
2020-09-09 17:54:54 -0500

    Add files via upload
    if you are running this through command line gives a little progress bar, as it'll take a couple minutes and i like looking at something
    you will need to have the tqdm module installed https://github.com/tqdm/tqdm

---
+++ binary_to_float-tqdm.py
@@ -0,0 +1,63 @@
+import struct
+import sys
+from tqdm import tqdm
+
+#initialize the 'fileName' list with file names for, xi, eta, Dg, and GH
+fileName = "", "", "", ""
+fileContent = []
+numbytes = []
+for z in fileName:
+	with open(z, mode='rb') as file: 
+		v = file.read()
+		fileContent.append(v)
+		numbytes.append(int(len(v)/4))#the four comes from the buffer size of float byte
+
+
+
+#the below variables of xi, eta, Dg, GH are the parsed float values
+xi = struct.unpack(str(numbytes[0])  + 'f', fileContent[0][0:4*int(numbytes[0])])
+eta = struct.unpack(str(numbytes[1])  + 'f', fileContent[1][0:4*int(numbytes[1])])
+Dg = struct.unpack(str(numbytes[2])  + 'f', fileContent[2][0:4*int(numbytes[2])])
+GH = struct.unpack(str(numbytes[3])  + 'f', fileContent[3][0:4*int(numbytes[3])])
+
+
+#the below portion is just for writing the floats to text files, it takes a couple of minutes for each file
+ans = input("Write data to txt file? (y/n)")
+
+if (ans == 'y'):
+	xi_data = open(fileName[0].replace('.', '')+'.txt','w')
+	xi_data.write('\n')
+	for x in tqdm(xi):
+		xi_data.write(str(x))
+		xi_data.write('\n')
+		
+	xi_data.close()
+
+
+	eta_data = open(fileName[1].replace('.', '')+'.txt','w')
+	eta_data.write('\n')
+	for x in tqdm(eta):
+		eta_data.write(str(x))
+		eta_data.write('\n')
+		
+	eta_data.close()
+
+
+	Dg_data = open(fileName[2].replace('.', '')+'.txt','w')
+	Dg_data.write('\n')
+	for x in tqdm(Dg):
+		Dg_data.write(str(x))
+		Dg_data.write('\n')
+	
+	Dg_data.close()
+
+
+	GH_data = open(fileName[3].replace('.', '')+'.txt','w')
+	GH_data.write('\n')
+	for x in tqdm(GH):
+		GH_data.write(str(x))
+		GH_data.write('\n')
+	
+	GH_data.close()
+
+sys.exit()
